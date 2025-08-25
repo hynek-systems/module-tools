e44b5394980a824b78eafe16d9d716759af404fd
Henrik Söderlind <henriksoderlind@MacBook-Pro-som-tillhor-Henrik-2.local>
2025-08-25 12:47:57 +0200

    Create service provider

---
+++ src/HynekModuleToolsServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Hynek\HynekModuleTools;
+
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+abstract class HynekModuleToolsServiceProvider extends PackageServiceProvider
+{
+
+}
